b77e0dbeb4747cdf036eaf5b23c27aa865caeb29
Akihiro Oyamada <admin@yomotsu.net>
2025-05-28 12:44:46 +0900

    update

--- middleware.js
+++ middleware.js
@@ -18,13 +18,3 @@ export function middleware(request) {
 
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - .swa (Azure static web apps)
-     */
-    '/((?!.swa).*)',
-  ],
-}
